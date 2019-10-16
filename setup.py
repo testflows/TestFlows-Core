96b96d5c1f219dd0ddfb35cac35708384ba82f62
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2019-10-16 18:08:28 -0400

    Updating setup.py to prepare for publishing.

--- setup.py
+++ setup.py
@@ -19,7 +19,14 @@ setup(
     description="TestFlows - Core",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
-    url="http://testflows.com",
+    url="https://github.com/testflows/testflows-core",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
     license="Apache-2.0",
     packages=[
         "testflows.core",
