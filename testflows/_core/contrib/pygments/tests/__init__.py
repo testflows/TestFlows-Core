31f8e0810e5ad2a05587d8f2e60e5057f89236c1
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-01-28 12:48:26 -0500

    Triming pygments contrib package.

--- testflows/_core/contrib/pygments/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    Pygments test package
-    ~~~~~~~~~~~~~~~~~~~~~
-
-    :copyright: Copyright 2006-2019 by the Pygments team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
-"""
--- testflows/_core/contrib/pygments/tests/support/html_formatter.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from pygments.formatters import HtmlFormatter
-
-
-class HtmlFormatterWrapper(HtmlFormatter):
-    name = 'HtmlWrapper'
