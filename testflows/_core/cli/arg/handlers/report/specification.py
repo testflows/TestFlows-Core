956310929fda57acaffd5dabde11cc0cfde3d624
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-07-09 16:24:44 -0400

    Fixing missing base64 import.

--- testflows/_core/cli/arg/handlers/report/specification.py
+++ testflows/_core/cli/arg/handlers/report/specification.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import re
+import base64
 import textwrap
 
 from collections import OrderedDict
