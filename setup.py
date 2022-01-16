54cbd0be6e74f701bd3604d0c021689eb6cfc1a0
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-01-16 17:20:21 -0500

    Adding aiomsg library to _core/contrib.

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         "testflows._core.contrib.yaml",
         "testflows._core.contrib.schema",
         "testflows._core.contrib.x256",
+        "testflows._core.contrib.aiomsg",
         "testflows._core.parallel",
         "testflows._core.parallel.executor",
         "testflows._core.utils",
---
+++ testflows/_core/contrib/aiomsg/msgproto.py
@@ -0,0 +1,33 @@
+"""
+aiomsg.msgproto
+===============
+
+These are messaging protocols
+
+"""
+
+import logging
+from asyncio import StreamReader, StreamWriter
+
+logger = logging.getLogger(__name__)
+_PREFIX_SIZE = 4
+
+
+async def read_msg(reader: StreamReader) -> bytes:
+    """ Returns b'' if the connection is lost."""
+    try:
+        size_bytes = await reader.readexactly(_PREFIX_SIZE)
+        size = int.from_bytes(size_bytes, byteorder="big")
+        data = await reader.readexactly(size)
+        logger.debug(f'Got data from socket: "{data[:64]}"')
+        return data
+    except (EOFError, OSError) as e:
+        logger.info(f"Connection lost: {e}")
+        return b""
+
+
+async def send_msg(writer: StreamWriter, data: bytes):
+    writer.write(len(data).to_bytes(4, byteorder="big"))
+    writer.write(data)
+    logger.debug(f'Wrote data to the socket: "{data[:64]}"')
+    await writer.drain()
