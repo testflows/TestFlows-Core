2594b494019ef0f0ec7590d351a5b67a2e1c6b10
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-05-30 16:10:26 -0400

    Adding new test for debugging.

--- tests/parallel_terminate.py
+++ tests/parallel_terminate.py
@@ -1,4 +1,5 @@
 import time
+import random
 import asyncio
 
 from testflows.core import *
@@ -39,7 +40,6 @@ def scenario(self, sleep=None, result_type=OK):
     with Then("I set the result"):
         result(type=result_type)
 
-
 @TestScenario
 async def async_scenario(self, sleep=None, result_type=OK):
     async with Given("I perform setup"):
@@ -59,33 +59,52 @@ async def async_scenario(self, sleep=None, result_type=OK):
 @TestSuite
 def suite(self, executor, with_join=False):
     with executor(5) as pool:
-        Scenario(test=scenario, name=f"scenario - pass", parallel=True, executor=pool)(sleep=2, result_type=OK)
-        Scenario(test=scenario, name=f"scenario - fail", parallel=True, executor=pool)(sleep=1, result_type=Fail)
+        for i in range(1):
+            Scenario(test=scenario, name=f"{i} scenario - pass", parallel=True, executor=pool)(sleep=1, result_type=OK)
+            Scenario(test=scenario, name=f"{i} scenario - fail", parallel=True, executor=pool, flags=TE)(sleep=1, result_type=Fail)
+            Scenario(test=scenario, name=f"{i} scenario - pass 2", parallel=True, executor=pool)(sleep=1, result_type=OK)
+            Scenario(test=scenario, name=f"{i} scenario - pass 3", parallel=True, executor=pool)(sleep=1, result_type=OK)
+            Scenario(test=scenario, name=f"{i} scenario - pass 4", parallel=True, executor=pool)(sleep=1, result_type=OK)
         if with_join:
-            join()
+           join()
 
 @TestSuite
 async def async_suite(self, executor, with_join=False):
     with executor(5) as pool:
-        Scenario(test=async_scenario, name=f"scenario - pass", parallel=True, executor=pool)(sleep=2, result_type=OK)
+        Scenario(test=async_scenario, name=f"scenario - pass", parallel=True, executor=pool)(sleep=1, result_type=OK)
         Scenario(test=async_scenario, name=f"scenario - fail", parallel=True, executor=pool)(sleep=1, result_type=Fail)
+        Scenario(test=async_scenario, name=f"scenario - pass 2", parallel=True, executor=pool)(sleep=1, result_type=OK)
+        Scenario(test=async_scenario, name=f"scenario - pass 3", parallel=True, executor=pool)(sleep=1, result_type=OK)
+        Scenario(test=async_scenario, name=f"scenario - pass 4", parallel=True, executor=pool)(sleep=1, result_type=OK)
+        
         if with_join:
             await join()
+
+@TestFeature
+def suites(self, executor, workers=10, with_join=False):
+    with executor(workers) as pool:
+        kwargs = {"flags": TE, "parallel": True, "executor": pool}
+        Suite(name="suite with ThreadPool join=True", test=suite, **kwargs)(executor=ThreadPool, with_join=True)
+        Suite(name="suite with ThreadPool join=False", test=suite, **kwargs)(executor=ThreadPool, with_join=False)
+        Suite(name="suite with ProcessPool join=True", test=suite, **kwargs)(executor=ProcessPool, with_join=True)
+        Suite(name="suite with ProcessPool join=False", test=suite, **kwargs)(executor=ProcessPool, with_join=False)
+        Suite(name="async suite with AsyncPool join=True", test=async_suite, **kwargs)(executor=AsyncPool, with_join=True)
+        Suite(name="async suite with AsyncPool join=False", test=async_suite, **kwargs)(executor=AsyncPool, with_join=False)
+        Suite(name="async suite with ThreadPool join=True", test=async_suite, **kwargs)(executor=ThreadPool, with_join=True)
+        Suite(name="async suite with ThreadPool join=False", test=async_suite, **kwargs)(executor=ThreadPool, with_join=False)
+        Suite(name="async suite with ProcessPool join=True", test=async_suite, **kwargs)(executor=ProcessPool, with_join=True)
+        Suite(name="async suite with ProcessPool join=False", test=async_suite, **kwargs)(executor=ProcessPool, with_join=False)
+        if with_join:
+            join()
 
 @TestModule
 def feature(self):
     """Check parallel test termination.
     """
-    Suite(name="suite with ThreadPool join=True", test=suite, flags=TE)(executor=ThreadPool, with_join=True)
-    Suite(name="suite with ThreadPool join=False", test=suite, flags=TE)(executor=ThreadPool, with_join=False)
-    Suite(name="suite with ProcessPool join=True", test=suite, flags=TE)(executor=ProcessPool, with_join=True)
-    Suite(name="suite with ProcessPool join=False", test=suite, flags=TE)(executor=ProcessPool, with_join=False)
-    Suite(name="async suite with AsyncPool join=True", test=async_suite, flags=TE)(executor=AsyncPool, with_join=True)
-    Suite(name="async suite with AsyncPool join=False", test=async_suite, flags=TE)(executor=AsyncPool, with_join=False)
-    Suite(name="async suite with ThreadPool join=True", test=async_suite, flags=TE)(executor=ThreadPool, with_join=True)
-    Suite(name="async suite with ThreadPool join=False", test=async_suite, flags=TE)(executor=ThreadPool, with_join=False)
-    Suite(name="async suite with ProcessPool join=True", test=async_suite, flags=TE)(executor=ProcessPool, with_join=True)
-    Suite(name="async suite with ProcessPool join=False", test=async_suite)(executor=ProcessPool, with_join=False)
+    for i in range(1):
+        start_time = time.time()
+        r = Suite(f"suites executed with ThreadPool join=True #{i}", test=suites, flags=TE)(executor=ThreadPool, workers=5, with_join=True)
+    #Suite("suites executed with ThreadPool join=False", test=suites, flags=TE)(executor=ThreadPool, with_join=False)
 
 if main():
     feature()
