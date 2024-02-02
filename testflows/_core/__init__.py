f674006db890b463c08ca21bf321d8642b1e93ff
vzakaznikov <41681088+vzakaznikov@users.noreply.github.com>
2024-02-01 19:01:48 -0500

    Update __init__.py to fix https://github.com/testflows/TestFlows-Core/issues/6

--- testflows/_core/__init__.py
+++ testflows/_core/__init__.py
@@ -35,5 +35,5 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-See testflows/_core/contrib folder in sources for lincenses of each third-party module.
+See testflows/_core/contrib folder in sources for licenses of each third-party module.
 """
